c3d97033b26d1f99ecd7461d29519be037d93140
ariffurqon <arif.furqon@gmail.com>
2015-12-21 01:10:43 -0800

    inputs and state

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -1,42 +1,66 @@
 var React = require('react-native');
+var Button = require('../common/button');
 
 var {
-	View,
-	Text,
-	StyleSheet,
-	TextInput
+  View,
+  Text,
+  StyleSheet,
+  TextInput
 } = React;
 
 module.exports = React.createClass({
-	render: function() {
-		return (<View style={styles.container}>
-				<Text>Sign In</Text>
-				<Text style={styles.label}>Username</Text>
-				<TextInput style={styles.input} />
-				<Text style={styles.label}>Password</Text>
-				<TextInput secureTextEntry={true} style={styles.input} />
-		</View>
-		)
-	}
+  getInitialState: function() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text>Sign In</Text>
+
+        <Text style={styles.label}>Username:</Text>
+        <TextInput
+          style={styles.input}
+          value={this.state.username}
+          onChangeText={(text) => this.setState({username: text})}
+          />
+
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => this.setState({password: text})}
+          />
+
+        <Button text={'Sign In'} onPress={this.onPress} />
+      </View>
+    );
+  },
+  onPress: function() {
+    // Log the user in
+  }
 });
 
-var styles = StyleSheet.create ({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	input: {
-		padding: 4,
-		height: 40,
-		borderColor: 'gray',
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 5,
-		width: 200,
-		alignSelf: 'center'
-	},
-	label: {
-		fontSize: 18
-	}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
+  },
+  label: {
+    fontSize: 18
+  }
 });
--- src/components/common/button.js
+++ src/components/common/button.js
@@ -1,38 +1,38 @@
-var React = require('react-native')
+var React = require('react-native');
 
 var {
-	Text,
-	StyleSheet,
-	TouchableHighlihgt
+  Text,
+  StyleSheet,
+  TouchableHighlight
 } = React;
 
 module.exports = React.createClass({
-	render: function(){
-		<TouchableHighlihgt 
-			style={styles.button}
-			underlayColor: {'gray'}
-			onPress={this.props.onPress}
-			>
-			<Text style={styles.buttonText}>
-				{this.props.text}
-			</Text>
-		</TouchableHighlihgt>
-	}
+  render: function() {
+    return (
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor={'gray'}
+        onPress={this.props.onPress}
+        >
+        <Text style={styles.buttonText}>{this.props.text}</Text>
+      </TouchableHighlight>
+    );
+  }
 });
 
 var styles = StyleSheet.create({
-	button: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderWidth: 1,
-		borderRadius: 5,
-		padding: 5,
-		borderColor: Black,
-		marginTop: 10
-	},
-	buttonText: {
-		flex: 1,
-		alignSelf: 'center',
-		fontSize: 20
-	}
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    borderColor: 'black',
+    marginTop: 10
+  },
+  buttonText: {
+    flex: 1,
+    alignSelf: 'center',
+    fontSize: 20
+  }
 });
