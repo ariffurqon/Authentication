95a79d76c59e585a6b37b9136555dc00a4e95656
ariffurqon <arif.furqon@gmail.com>
2015-12-21 22:22:26 -0800

    display username

--- src/components/photos/photos.js
+++ src/components/photos/photos.js
@@ -1,4 +1,5 @@
 var React = require('react-native');
+var Parse = require('parse/react-native');
 
 var {
 	View,
@@ -7,10 +8,25 @@ var {
 } = React;
 
 module.exports = React.createClass({
+	getInitialState: function(){
+		return{
+			user: null
+		}
+	},
+	componentWillMount: function(){
+		Parse.User.currentAsync()
+			.then((user) => {this.setState({user: user}); })
+	},
 	render: function(){
+		if (!this.state.user){
+			return <Text>Loading..</Text>
+		}
+
+		var username = this.state.user.get('username');
+
 		return (
 			<View style={styles.container}>
-				<Text>Welcome Back!</Text>
+				<Text>Welcome Back, {username}!</Text>
 			</View>
 		)
 	}
